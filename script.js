55f9067d24cb557729de514a4c51219dbb7c8751
peetu <peetu.storhammar@gmail.com>
2017-09-26 19:41:44 +0300

    toggleClass homma ja pari kissakuvaa

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showalert() {
 	alert("PÖÖÖÖÖÖÖ༼ つ ◕_◕ ༽つ.... säikäytinkö..... hehhehe(¬‿¬)....... heippa! ♪~ ᕕ(ᐛ)ᕗ");
 }
+
+function toggleClass(elemid, classname) {
+	let elem = document.getElementById(elemid);
+	elem.classList.toggle(classname);
+}
